7da7bee039b74f450ddccab594aa2b199def5d32
NoraCarey <46361714+NoraCarey@users.noreply.github.com>
2020-08-17 19:13:27 -0400

    Update 011. Weather Observation Station 6.sql

--- SQL/01. Basic Select/011. Weather Observation Station 6.sql
+++ SQL/01. Basic Select/011. Weather Observation Station 6.sql
@@ -1,6 +1,12 @@
 -- # Problem: https://www.hackerrank.com/challenges/weather-observation-station-6/problem
 
-
+-- # Method 1:
 SELECT DISTINCT CITY
 FROM STATION 
 WHERE LEFT(CITY, 1) IN ('a', 'e', 'i', 'o', 'u');
+
+
+-- # Method 2:
+SELECT DISTINCT CITY
+FROM STATION 
+WHERE CITY RLIKE '.*[aeiou]$';
