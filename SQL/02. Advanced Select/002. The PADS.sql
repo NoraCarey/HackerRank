256dbf99245fafc32cd2f1c9864b58f1d3431b92
NoraCarey <46361714+NoraCarey@users.noreply.github.com>
2020-12-17 13:09:39 -0500

    Update 002. The PADS.sql

--- SQL/02. Advanced Select/002. The PADS.sql
+++ SQL/02. Advanced Select/002. The PADS.sql
@@ -1,8 +1,18 @@
 -- # Problem: https://www.hackerrank.com/challenges/the-pads/problem
-
+Method 1:
 SELECT CONCAT(Name, '(', LEFT(Occupation, 1), ')')
 FROM OCCUPATIONS
 ORDER BY Name ASC;
+
+Method 2: 
+SELECT CASE
+           WHEN occupation = 'actor' THEN CONCAT(name, '(A)')
+           WHEN occupation = 'professor' THEN CONCAT(name, '(P)')
+           WHEN occupation = 'singer' THEN CONCAT(name, '(S)')
+           WHEN occupation = 'doctor' THEN CONCAT(name, '(D)')
+        END 
+FROM occupations
+ORDER BY name;
 
 SELECT CONCAT('There are a total of ', COUNT(Occupation), ' ', LOWER(Occupation), 's.')
 FROM OCCUPATIONS
