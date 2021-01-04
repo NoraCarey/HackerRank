ffdee6d480f3214e349b4e15a6fea866809b7678
NoraCarey <46361714+NoraCarey@users.noreply.github.com>
2021-01-04 13:45:03 -0500

    Update 003. Symmetric Pairs.sql

--- SQL/05. Advanced Join/003. Symmetric Pairs.sql
+++ SQL/05. Advanced Join/003. Symmetric Pairs.sql
@@ -8,8 +8,8 @@ GROUP BY f1.x, f1.y
 # after we found the symmetric paris, we could get a list, but we need to prevent duplication in this list
 # therefore, we need to consider two situations,
 # one: f1.x = f1.y ---- we use COUNT(f1.x) > 1 to prevent duplication
-# other: f1.x <> f1.y --- we only pulled out the pairs that satisty f1.x < f1.y 
-#                         while we verified (20, 21) and (21, 20) are symmetric pairs
+# other: f1.x <> f1.y --- we only pulled out the pairs that satisty f1.x < f1.y to prevent duplication
+#                         while we verified (20, 21) and (21, 20) are symmetric pairs, 
 HAVING COUNT(f1.x) > 1 OR f1.x < f1.y
 ORDER BY f1.x;
 
